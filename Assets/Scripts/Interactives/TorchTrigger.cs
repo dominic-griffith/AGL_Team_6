4127e63628aa96ee6e4fc666e301178fe5d29cce
dominic-griffith <dominicgriffith15@gmail.com>
2022-10-19 17:08:01 -0700

    Interactive Torch

---
+++ Assets/Scripts/Interactives/TorchTrigger.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TorchTrigger : MonoBehaviour
+{
+    private Animator anim;
+    public bool isOn = false;
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Fire")
+        {
+            TurnOnTorch();
+        }
+    }
+
+    private void TurnOnTorch()
+    {
+        anim.SetBool("Play", true);
+        //play torch sound
+        isOn = true;
+
+    }
+}
